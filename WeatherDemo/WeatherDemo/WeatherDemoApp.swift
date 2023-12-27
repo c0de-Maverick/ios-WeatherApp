ed3fd944237e6763409182b78397d03b57c5b583
Aditya kansara <adityakansara@Adityas-MacBook-Pro.local>
2023-12-27 19:58:17 +0530

    commit

---
+++ WeatherDemo/WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Aditya kansara on 25/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
